a844c00d9d88c09b3413ee67aae3fa0e04141845
David Andresky <dandresky@gmail.com>
2024-12-15 09:19:14 -0700

    debugging with hardware

--- src/DAC6050x.cpp
+++ src/DAC6050x.cpp
@@ -131,7 +131,7 @@ uint16_t DAC6050x::setup(void) {
 
     if(result == 0) {
         // result = set_dac_output(0, 0x7FF0);
-        result = write_register((uint8_t)DAC0_DATA_CMD + 0, 0x7FF);
+        result = write_register((uint8_t)DAC0_DATA_CMD + 0, 0x7FF0);
     }
     // if(result == 0) {
     //     result = set_dac_output(1, 0x7FF0);
