677ee40ccd8dc7074318a5d1a7ab075a2a6b2c7a
David Andresky <dandresky@gmail.com>
2024-12-15 08:46:48 -0700

    write mid-scale for debug

--- src/DAC6050x.cpp
+++ src/DAC6050x.cpp
@@ -129,12 +129,12 @@ uint16_t DAC6050x::setup(void) {
         }
     }
 
-    if(result == 0) {
-        result = set_dac_output(0, 0x7FF0);
-    }
-    if(result == 0) {
-        result = set_dac_output(1, 0x7FF0);
-    }
+    // if(result == 0) {
+    //     result = set_dac_output(0, 0x7FF0);
+    // }
+    // if(result == 0) {
+    //     result = set_dac_output(1, 0x7FF0);
+    // }
 
     _wire->end();
     
